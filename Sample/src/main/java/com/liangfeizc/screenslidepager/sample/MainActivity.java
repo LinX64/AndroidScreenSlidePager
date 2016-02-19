d3bd191a6987817f3a1428884c6314e1655c4327
LinX64 <ash.wxrz@hotmail.com>
2016-02-19 15:31:25 +0330

    Update MainActivity.java

--- Sample/src/main/java/com/liangfeizc/screenslidepager/sample/MainActivity.java
+++ Sample/src/main/java/com/liangfeizc/screenslidepager/sample/MainActivity.java
@@ -13,13 +13,12 @@ import com.liangfeizc.slidepager.SlidePagerActivity;
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String[] IMAGES = new String[] {
+    private static final String[] IMAGES = new String[]{
             "http://img1.cache.netease.com/catchpic/B/B2/B2F274C1CCD5A89133261E6252A0C8E9.jpg",
             "http://img5.duitang.com/uploads/item/201408/09/20140809204759_CwtQN.jpeg",
             "http://img5q.duitang.com/uploads/item/201204/06/20120406151343_HefME.jpeg",
             "http://images.qianlong.com/mmsource/imghylanda/201108/25/21/7131919880274907937.jpg",
-            "http://imgsrc.baidu.com/forum/pic/item/78370e46f91582056a63e576.jpg"
-    };
+            "http://imgsrc.baidu.com/forum/pic/item/78370e46f91582056a63e576.jpg"};
 
 
     @Override
